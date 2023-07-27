e6a953c5ea7699d389eab8781c6d7b10aede1f0c
Truong Thuc Van <truongthucvan1242@gmail.com>
2023-07-27 15:26:08 +0700

    feat: Adding Dummy Expense Data

--- screens/AllExpenses.js
+++ screens/AllExpenses.js
@@ -1,7 +1,7 @@
-import { Text } from "react-native";
+import ExpensesOutput from "../components/ExpensesOutput/ExpensesOutput";
 
-function AllExpenses() {
-  return <Text>AllExpenses Screen</Text>;
+function RecentExpenses() {
+  return <ExpensesOutput expensesPeriod="Last 7 Days" />;
 }
 
-export default AllExpenses;
+export default RecentExpenses;
